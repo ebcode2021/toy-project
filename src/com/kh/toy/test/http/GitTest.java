01fa41042af005e6184e262890fc5dab031df77e
son <ebcode2021@gmail.com>
2021-09-15 15:10:26 +0900

    conflict 2nd test

--- src/com/kh/toy/test/http/GitTest.java
+++ src/com/kh/toy/test/http/GitTest.java
@@ -1,9 +1,9 @@
 package com.kh.toy.test.http;
 
 public class GitTest {
-	private static final String DESC ="GIT테스트용 클래스";
+	private static final String DESC ="local GIT테스트용 클래스";
 	private static final String CREDENTIAL ="GIT테스트용 클래스";
-	private static final String GITHUB ="원격리포지토리에서 직접 수정!";
+	private static final String GITHUB ="local 원격리포지토리에서 직접 수정!";
 	private static final String CONFLICT ="Conflict 테스트";
-	private static final String DEV ="DEV 브랜치에서 작업";
+	private static final String DEV ="local DEV 브랜치에서 작업";
 }
