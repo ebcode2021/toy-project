2bd21191706fd74eb76ed4af2b7154ce81c31008
son <ebcode2021@gmail.com>
2021-09-15 15:01:54 +0900

    [손은비] Local Conflict Test

--- src/com/kh/toy/test/http/GitTest.java
+++ src/com/kh/toy/test/http/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
 	private static final String DESC ="GIT테스트용 클래스";
 	private static final String CREDENTIAL ="GIT테스트용 클래스";
 	private static final String GITHUB ="원격리포지토리에서 직접 수정!";
+	private static final String LOCALCONFLICT ="local에서 conflict 테스트";
 	private static final String DEV ="DEV 브랜치에서 작업";
 }
