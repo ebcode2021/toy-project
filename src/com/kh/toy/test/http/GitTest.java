4b62f27e501756d826958f42e133e37d1e3a0961
ebcode2021 <84271971+ebcode2021@users.noreply.github.com>
2021-09-15 14:52:39 +0900

    Update GitTest.java
    [원격리포지토리] 변경

--- src/com/kh/toy/test/http/GitTest.java
+++ src/com/kh/toy/test/http/GitTest.java
@@ -3,5 +3,5 @@ package com.kh.toy.test.http;
 public class GitTest {
 	private static final String DESC ="GIT테스트용 클래스";
 	private static final String CREDENTIAL ="GIT테스트용 클래스";
-	private static final String GITHUB ="원격리포지토리에서 직접 수정";
+	private static final String GITHUB ="원격리포지토리에서 직접 수정!";
 }
